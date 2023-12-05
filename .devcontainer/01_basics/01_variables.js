5e6c351ad43311f254ac999d2331aa8b24cae1a6
abhay gupta <51904700+abhaygupta7865@users.noreply.github.com>
2023-12-05 12:51:54 +0000

    container config done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accId= 123
+let email="ab@gmail.com"
+var password="123"
+city="bhopal"
+
+// accId=4
+email="abcd@gmail.com"
+password="343"
+city="spn"
+console.log(accId)
+console.table([accId,email,password,city])
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; //use all JS code as a newer version 
+// alert(3+3) it will give error because we are using nodejs not browser..browser using have html and js engine combined
+
+//numbers 
+//bigint
+//string
+//boolean
+//null
+//undefined
+//symbol
+//object
