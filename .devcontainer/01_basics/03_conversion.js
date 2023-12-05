a6546998836ff4dec8d9434387b09a3941535827
abhay gupta <51904700+abhaygupta7865@users.noreply.github.com>
2023-12-05 13:17:34 +0000

    conversion of datatype

---
+++ .devcontainer/01_basics/03_conversion.js
@@ -0,0 +1,13 @@
+let score ="33" //we know its datatype but when values come from frontend then we are not knowing its datatype
+console.log(typeof score)
+console.log(typeof(score))
+
+let val=Number(score)
+console.log(typeof val)
+console.log(val)
+
+let score2="33abc"
+console.log(typeof(score2))
+let val2=Number(score2)
+console.log(typeof val2)
+console.log(val2)
