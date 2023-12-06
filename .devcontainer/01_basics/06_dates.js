7818c79028d9c7fd8c09eb23cdbc646db5f9b721
abhay gupta <51904700+abhaygupta7865@users.noreply.github.com>
2023-12-06 16:47:08 +0000

    objects in javascript

---
+++ .devcontainer/01_basics/06_dates.js
@@ -0,0 +1,6 @@
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toTimeString());
