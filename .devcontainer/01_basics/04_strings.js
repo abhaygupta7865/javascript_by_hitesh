5f04ed2fad40372ff9c32710660e8822d599f4b8
abhay gupta <51904700+abhaygupta7865@users.noreply.github.com>
2023-12-06 14:19:06 +0000

    string methods

---
+++ .devcontainer/01_basics/04_strings.js
@@ -0,0 +1,18 @@
+const string1= "abhay1"
+const string2= 'abhay2'
+const string3= `abhay3`
+const string4= new String("hello")
+console.log(`my name is ${string1}`)
+console.log(string4.toUpperCase())
+console.log(string4[0])
+console.log(string4.__proto__);
+console.log(string4.charAt(3))
+console.log(string4.indexOf(2))
+
+const news= string4.substring(0,2)
+console.log(news);
+
+const news2= string4.slice(-4,4)
+console.log(news2);
+
+
