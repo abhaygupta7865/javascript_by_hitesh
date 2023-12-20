ef3bd70ec20a6bc4495754a37df28b2304bba70d
Abhay <abhay.gupta7865@gmail.com>
2023-12-20 14:26:51 +0530

    project 2

--- project/02_BMIcalculator/p2script.js
+++ project/02_BMIcalculator/p2script.js
@@ -4,16 +4,16 @@ Bmi.addEventListener('submit',function(e){
     e.preventDefault();
     const height = parseInt(document.querySelector('#height').value);
     const weight = parseInt(document.querySelector('#weight').value);
-    const results = document.querySelector('#results');
+    const result = document.querySelector('#results');
 
     if(height ==='' || height<0 || isNaN(height)){
-        results.innerHTML= `please give valid Input`;
+        result.innerHTML= `please give valid Input`;
     }
     else if (weight ==='' || weight<0 || isNaN(weight)){
-        results.innerHTML= `please give valid Input`;
+        result.innerHTML= `please give valid Input`;
     }
     else {
         const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-        results.innerHTML = `<span>${bmi}</span>`
+        result.innerHTML = `<span>${bmi}</span>`
     }
-})
+});
